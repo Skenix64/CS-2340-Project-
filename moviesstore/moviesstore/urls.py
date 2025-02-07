9471f7511ba142d18e67547ecf2af62ff566c31f
tyler <tylerlu168@gmail.com>
2025-02-06 19:13:08 -0500

    Did the website framework.

--- moviesstore/moviesstore/urls.py
+++ moviesstore/moviesstore/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls')),
+    path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
+    #path('cart/', include('cart.urls')),
 ]
