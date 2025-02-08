af02a081457b3dcdb5f8609eb222d907bde7deb4
Sarthak Das <dsarthak19@gmail.com>
2025-02-08 04:05:41 -0500

    Finished add to cart, and remove to cart?

--- moviesstore/cart/views.py
+++ moviesstore/cart/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-#from django.shortcuts import get_object_or_404, redirect
-#from movies.models import Movie
- #def add(request, id):
-  #   get_object_or_404(Movie, id=id)
-   #  cart = request.session.get('cart', {})
-    # cart[id] = request.POST['quantity']
-     #request.session['cart'] = cart
-     #return redirect('home.index')
+from django.shortcuts import get_object_or_404, redirect
+from movies.models import Movie
+def add(request, id):
+     get_object_or_404(Movie, id=id)
+     cart = request.session.get('cart', {})
+     cart[id] = request.POST['quantity']
+     request.session['cart'] = cart
+     return redirect('home.index')
+
+def clear(request):
+    request.session['cart'] = {}
+    return redirect('cart.index')
