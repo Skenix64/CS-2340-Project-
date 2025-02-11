098210a0c4bfd5aaddd436b6d54c3984675df7aa
clxirelee <clee931@gatech.edu>
2025-02-11 15:48:35 -0500

    Log in, reset password

---
+++ moviesstore/accounts/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+@login_required
+
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
