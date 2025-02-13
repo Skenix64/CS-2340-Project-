bd7ecd8bd86d46bb5c054ca0bd5c459f9e836ac3
Tyler Lu <tylerlu168@gmail.com>
2025-02-13 15:50:16 -0500

    Completed the website
    We finished!!!

--- moviesstore/accounts/urls.py
+++ moviesstore/accounts/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
+    path('orders/', views.orders, name='accounts.orders'),
 
 ]
